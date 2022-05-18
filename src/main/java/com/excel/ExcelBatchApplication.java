8eb70f0cb49bb70b9e6b79004b640bed99d10be3
SHH <shhgrammer@gmail.com>
2022-05-18 09:15:14 +0900

    initial commit

---
+++ src/main/java/com/excel/ExcelBatchApplication.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelBatchApplication.class, args);
+	}
+
+}
